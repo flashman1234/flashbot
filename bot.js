b13a4a1d00111777ec31331ea899d26e759d64c9
Michal <flashman1234@gmail.com>
2020-12-19 10:42:29 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,16 +2,6 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
- 
-
-client.on('ready', () => {
-
-    console.log('I am ready!');
-
-});
-
- 
-
 client.on('message', message => {
 
     if (message.content.toLowerCase().startsWith('flashbot')) {
@@ -22,8 +12,4 @@ client.on('message', message => {
 
 });
 
- 
-
-// THIS  MUST  BE  THIS  WAY
-
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN);
